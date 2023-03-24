2a7fd2bb132a1001cb75cac5883bcc948d73557e
yoko-newDeveloper <whitesnow.wave@gmail.com>
2023-03-24 22:40:31 +0900

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import HelloWorld from '../components/HelloWorld.vue'
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class HomeView extends Vue {}
+@Component({ components: { HelloWorld } })
+export default class HomeView extends Vue {
+}
 </script>
